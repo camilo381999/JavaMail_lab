735d27453f517dcef5d4faf5523d639458ecce8a
camilo381999 <camilo-leguizamon1@upc.edu.co>
2021-01-27 18:31:06 -0500

    Initial commit

--- app/src/main/java/co/edu/unipiloto/javamail/MainActivity.java
+++ app/src/main/java/co/edu/unipiloto/javamail/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,7 +27,8 @@ import javax.mail.internet.MimeMessage;
 
 public class MainActivity extends AppCompatActivity {
     //variables
-    EditText etTo,etSubject,etMessage;
+    EditText etFrom,etTo,etSubject,etMessage;
+    String from,to,subject,message;
     Button btSend;
     String sEmail="calf381999@gmail.com";
     String sPassword="";
@@ -36,109 +38,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //asignacion de variables
-        etTo=(EditText)findViewById(R.id.et_to);
-        etSubject=(EditText)findViewById(R.id.et_subject);
-        etMessage=(EditText)findViewById(R.id.et_message);
         btSend=(Button)findViewById(R.id.bt_send);
-
-        //Sender email
-
-
-
         btSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //inicializa propiedades
-                Properties properties=new Properties();
-                properties.put("mail.smtp.auth","true");
-                properties.put("mail.smtp.starttls.enable","true");
-                properties.put("mail.smtp.hpst","smtp.gmail.com");
-                properties.put("mail.smtp.port","587");
+                final ProgressDialog pd=ProgressDialog.show(MainActivity.this,
+                        "Sending",
+                        "Sending Gmail Java api email...",
+                        true,false);
+                new Thread((Runnable) () -> {
+                    try {
+                        //asignacion de variables
+                        etFrom=(EditText)findViewById(R.id.et_from);
+                        etTo=(EditText)findViewById(R.id.et_to);
+                        etSubject=(EditText)findViewById(R.id.et_subject);
+                        etMessage=(EditText)findViewById(R.id.et_message);
 
-                //inicializa sesion
-                Session session=Session.getInstance(properties, new Authenticator() {
-                    @Override
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(sEmail,sPassword);
+                        from=etFrom.getText().toString();
+                        to=etTo.getText().toString();
+                        subject=etSubject.getText().toString();
+                        message=etMessage.getText().toString();
+
+                        GMailSender sender = new GMailSender(sEmail,sPassword);
+                        sender.sendMail(subject,message,from,to);
+                    } catch (Exception e) {
+                        Log.e("SendMail",e.getMessage(),e);
                     }
-                });
-
-
-                try {
-                    //inicializa contenido del email
-                    Message message = new MimeMessage(session);
-
-                    //enviador de email
-                    message.setFrom(new InternetAddress(sEmail));
-
-                    //recipiente email
-                    message.setRecipients(Message.RecipientType.TO,
-                            InternetAddress.parse(etTo.getText().toString().trim()));
-                    //Email subject
-                    message.setSubject(etSubject.getText().toString().trim());
-                    //Email Message
-                    message.setText(etMessage.getText().toString().trim());
-
-                    //send email
-                    new SendMail().execute(message);
-                } catch (MessagingException e) {
-                    e.printStackTrace();
-                }
+                    pd.dismiss();
+                }).start();
             }
         });
     }
 
-    private class SendMail extends AsyncTask<Message,String,String> {
-        //initialize progress dialog
-        private ProgressDialog progressDialog;
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            //create and show progress dialog
-            progressDialog=ProgressDialog.show(MainActivity.this,
-                    "Please wait","Sending mail...",true,false);
-        }
-
-        @Override
-        protected String doInBackground(Message... messages) {
-            try {
-                Transport.send(messages[0]);
-                return "Success";
-            } catch (MessagingException e) {
-                e.printStackTrace();
-                return "Error";
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-
-            progressDialog.dismiss();
-            if(s.equals("Success")){
-
-
-                AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
-                builder.setCancelable(false);
-                builder.setTitle(Html.fromHtml("<font color='#509324'>Success</font>"));
-                builder.setMessage("Mail send successfully.");
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-
-                        etTo.setText("");
-                        etSubject.setText("");
-                        etMessage.setText("");
-                    }
-                });
-                builder.show();
-            }else{
-                Toast.makeText(getApplicationContext(),
-                        "Something went wrong ?",Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
 }
